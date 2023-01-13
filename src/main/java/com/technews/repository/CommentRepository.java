4a99c347371dab2b40965c2c1ddcc6c07f1cc2ab
HankHill <hhc3h8@gmail.com>
2023-01-13 10:39:52 -0600

    "Front-End controllers up and working.

--- src/main/java/com/technews/repository/CommentRepository.java
+++ src/main/java/com/technews/repository/CommentRepository.java
@@ -1,4 +1,5 @@
 package com.technews.repository;
+
 import com.technews.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findAllCommentsByPostId(Integer id) throws Exception;
+
+    List<Comment> findAllCommentsByPostId(int postId);
 }
